b870538f1a23832ed4a737f82fd7f2dde8ec451d
CatalinaMonserrat <kata1800@gmail.com>
2025-10-14 17:15:12 -0300

    Se agregan clases a models.py

--- biblioteca/productos/models.py
+++ biblioteca/productos/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Autor(models.Model):
     nombre = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
+    
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
     def __str__(self):
         return self.nombre
     
@@ -13,3 +19,24 @@ class Libro(models.Model):
     stock   = models.PositiveIntegerField(default=0)
     def __str__(self):
         return self.titulo
+
+class Pedido(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    direccion = models.CharField(max_length=200)
+    creando_en = models.DateTimeField(auto_now_add=True)
+    def total (self):  #funcion para calcular el total
+        return sum(item.subtotal() for item in self.items.all())
+    def __str__(self):
+        return f"Pedido #{self.id} de {self.usuario}"
+    
+class LineaPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    libro = models.ForeignKey(Libro, on_delete=models.PROTECT)
+    cantidad = models.PositiveIntegerField(default=1)
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    def subtotal(self):
+        return self.cantidad * self.precio_unitario
+    def __str__(self):
+        return f"{self.libro} x{self.cantidad}"
